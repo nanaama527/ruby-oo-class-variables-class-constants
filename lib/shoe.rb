2abbad1e769210fa2828d01a268ca74ec771ac39
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-25 02:08:55 -0500

    Update shoe.rb

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,11 +4,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+      BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
